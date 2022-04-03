124944e08438fc4c4b4be900158d978f93b7cf5c
cmondi18 <disivolap@gmail.com>
2022-04-03 15:35:00 +0300

    feat: add answers controller

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -31,6 +31,7 @@ RSpec.describe QuestionsController, type: :controller do
 
   describe 'GET #new' do
     before { get :new }
+
     it 'assigns a new Question to @question' do
       expect(assigns(:question)).to be_a_new(Question)
     end

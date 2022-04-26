fdde886a6a845976ae50ddd1de0edcfee43298ad
cmondi18 <disivolap@gmail.com>
2022-04-26 15:26:19 +0300

    feat: add upload image(s) to question

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,7 @@ class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
   belongs_to :user
   belongs_to :best_answer, required: false, class_name: 'Answer', dependent: :destroy, optional: true
+  has_many_attached :files
 
   validates :title, :body, presence: true
 

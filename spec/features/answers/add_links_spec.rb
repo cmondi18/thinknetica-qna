4b4f7baa92150db5e4a3a7fe979b3ff95f1b7a9c
cmondi18 <disivolap@gmail.com>
2022-04-28 13:04:44 +0700

    feat: add url validation to link

--- spec/features/answers/add_links_spec.rb
+++ spec/features/answers/add_links_spec.rb
@@ -8,37 +8,54 @@ feature 'User can add links to answer', %q{
 
   given(:user) { create(:user) }
   given(:question) { create(:question) }
-  given(:gist_url) { 'https://gist.github.com/cmondi18/c4c1f91a73c03cf9dfb32792e856e025' }
-  given(:second_gist_url) { 'https://gist.github.com/cmondi18/88d28a96b0add346cb73a3daa36302ce' }
 
   before do
     sign_in(user)
     visit question_path(question)
-
-    fill_in 'Body', with: 'text text text'
-    fill_in 'Link name', with: 'My Gist'
-    fill_in 'Url', with: gist_url
   end
 
-  scenario 'User adds link when give an answer', js: true do
-    click_on 'Submit answer'
+  describe 'User adds' do
+    given(:gist_url) { 'https://gist.github.com/cmondi18/c4c1f91a73c03cf9dfb32792e856e025' }
+    given(:second_gist_url) { 'https://gist.github.com/cmondi18/88d28a96b0add346cb73a3daa36302ce' }
 
-    within '.answer' do
-      expect(page).to have_link 'My Gist', href: gist_url
+    before do
+      fill_in 'Body', with: 'text text text'
+      fill_in 'Link name', with: 'My Gist'
+      fill_in 'Url', with: gist_url
+    end
+
+    scenario 'link when give an answer', js: true do
+      click_on 'Submit answer'
+
+      within '.answer' do
+        expect(page).to have_link 'My Gist', href: gist_url
+      end
+    end
+
+    scenario 'two links when give an answer', js: true do
+      click_on 'add link'
+
+      first(:field, 'Link name').fill_in with: 'My Second Gist'
+      first(:field, 'Url').fill_in with: second_gist_url
+
+      click_on 'Submit answer'
+
+      within '.answer' do
+        expect(page).to have_link 'My Gist', href: gist_url
+        expect(page).to have_link 'My Second Gist', href: second_gist_url
+      end
     end
   end
 
-  scenario 'User adds two links when give an answer', js: true do
-    click_on 'add link'
-
-    first(:field, 'Link name').fill_in with: 'My Second Gist'
-    first(:field, 'Url').fill_in with: second_gist_url
+  scenario 'User adds link with incorrect URL when give an answer', js: true do
+    fill_in 'Body', with: 'text text text'
+    fill_in 'Link name', with: 'My Gist'
+    fill_in 'Url', with: 'wrongURL'
 
     click_on 'Submit answer'
 
-    within '.answer' do
-      expect(page).to have_link 'My Gist', href: gist_url
-      expect(page).to have_link 'My Second Gist', href: second_gist_url
+    within '.answer-errors' do
+      expect(page).to have_text 'Links url is not a valid HTTP URL'
     end
   end
 end

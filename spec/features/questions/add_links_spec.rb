1a3e267b6b868c82866423f6a1df3d5c0c954a18
cmondi18 <disivolap@gmail.com>
2022-04-28 13:04:44 +0700

    feat: add url validation to link

--- spec/features/questions/add_links_spec.rb
+++ spec/features/questions/add_links_spec.rb
@@ -7,34 +7,49 @@ feature 'User can add links to question', %q{
 } do
 
   given(:user) { create(:user) }
-  given(:gist_url) { 'https://gist.github.com/cmondi18/c4c1f91a73c03cf9dfb32792e856e025' }
-  given(:second_gist_url) { 'https://gist.github.com/cmondi18/88d28a96b0add346cb73a3daa36302ce' }
 
   before do
     sign_in(user)
     visit new_question_path
+  end
 
+  describe 'User adds' do
+    given(:gist_url) { 'https://gist.github.com/cmondi18/c4c1f91a73c03cf9dfb32792e856e025' }
+    given(:second_gist_url) { 'https://gist.github.com/cmondi18/88d28a96b0add346cb73a3daa36302ce' }
+
+    before do
+      fill_in 'Title', with: 'Test question'
+      fill_in 'Body', with: 'text text text'
+      fill_in 'Link name', with: 'My Gist'
+      fill_in 'Url', with: gist_url
+    end
+    scenario 'link when asks question' do
+      click_on 'Ask'
+
+      expect(page).to have_link 'My Gist', href: gist_url
+    end
+
+    scenario 'two links when asks question', js: true do
+      click_on 'add link'
+
+      first(:field, 'Link name').fill_in with: 'My Second Gist'
+      first(:field, 'Url').fill_in with: second_gist_url
+
+      click_on 'Ask'
+
+      expect(page).to have_link 'My Gist', href: gist_url
+      expect(page).to have_link 'My Second Gist', href: second_gist_url
+    end
+  end
+
+  scenario 'User adds link with incorrect URL when asks question', js: true do
     fill_in 'Title', with: 'Test question'
     fill_in 'Body', with: 'text text text'
     fill_in 'Link name', with: 'My Gist'
-    fill_in 'Url', with: gist_url
-  end
-
-  scenario 'User adds link when asks question' do
-    click_on 'Ask'
-
-    expect(page).to have_link 'My Gist', href: gist_url
-  end
-
-  scenario 'User adds two links when asks question', js: true do
-    click_on 'add link'
-
-    first(:field, 'Link name').fill_in with: 'My Second Gist'
-    first(:field, 'Url').fill_in with: second_gist_url
+    fill_in 'Url', with: 'ftp://websiteaddress.com'
 
     click_on 'Ask'
 
-    expect(page).to have_link 'My Gist', href: gist_url
-    expect(page).to have_link 'My Second Gist', href: second_gist_url
+    expect(page).to have_text 'Links url is not a valid HTTP URL'
   end
 end
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -5,4 +5,14 @@ RSpec.describe Link, type: :model do
 
   it { should validate_presence_of :name }
   it { should validate_presence_of :url }
+
+  context 'validate url' do
+    it 'when url is correct' do
+      expect { create(:link, url: 'https://google.com/search?query=blabla').to_not raise_error(URI::InvalidURIError) }
+    end
+
+    it 'when url is incorrect' do
+      expect { create(:link, url: 'wrongURL').to raise_error(URI::InvalidURIError) }
+    end
+  end
 end

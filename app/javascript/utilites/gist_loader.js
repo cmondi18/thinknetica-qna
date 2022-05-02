15c4a6e9d481e867c64a4f29a404ce9596cef580
cmondi18 <disivolap@gmail.com>
2022-05-02 17:30:23 +0700

    feat: add latency to gist loader and update gists after add answer

--- app/javascript/utilites/gist_loader.js
+++ app/javascript/utilites/gist_loader.js
@@ -2,4 +2,13 @@ import gistLoader from "easy-gist-async";
 
 document.addEventListener('turbolinks:load', function () {
     gistLoader();
+
+    var el = document.querySelector('.new-answer input[type="submit"]');
+    el.addEventListener('click', loadGist)
 });
+
+
+// I'm not sure that it is best option, but it works.
+function loadGist() {
+    setTimeout(() => { gistLoader(); }, 1000);
+}

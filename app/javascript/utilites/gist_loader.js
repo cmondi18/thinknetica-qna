7b3ed1880d5ea8c483a747b5817454d1fcb24c99
cmondi18 <disivolap@gmail.com>
2022-05-04 12:22:21 +0700

    feat: add edit link func

--- app/javascript/utilites/gist_loader.js
+++ app/javascript/utilites/gist_loader.js
@@ -5,6 +5,12 @@ document.addEventListener('turbolinks:load', function () {
 
     var answerSubmitButton = document.querySelector('.new-answer input[type="submit"]');
     if (answerSubmitButton) answerSubmitButton.addEventListener('click', loadGist)
+
+    var questionEditSubmitButton = document.querySelector('form[id^="edit-question-"] input[type=submit]');
+    if (questionEditSubmitButton) questionEditSubmitButton.addEventListener('click', loadGist)
+
+    var answerEditSubmitButton = document.querySelector('form[id^="edit-answer-"] input[type=submit]');
+    if (answerEditSubmitButton) answerEditSubmitButton.addEventListener('click', loadGist)
 });
 
 

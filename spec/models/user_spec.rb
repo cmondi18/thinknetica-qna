a726612b8995f9dd69da9489639ae901ee0cc2e7
cmondi18 <disivolap@gmail.com>
2022-05-25 15:01:57 +0300

    specs: user able to vote

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -31,4 +31,36 @@ RSpec.describe User, type: :model do
       expect(user.rewards).to match_array(rewards)
     end
   end
+
+  context 'verification of user voting able' do
+    let(:user) { create(:user) }
+    let(:question) { create(:question) }
+    let(:voted_answer) { create(:answer) }
+    let(:own_question) { create(:question, user: user) }
+    let(:vote) { create(:vote, votable: voted_answer, user: user) }
+
+    it 'user is author of votable' do
+      expect(user).to_not be_able_to_vote(own_question)
+    end
+
+    it 'user is not author of votable' do
+      expect(user).to be_able_to_vote(question)
+    end
+
+    it 'user already voted' do
+      vote.reload
+
+      expect(user).to_not be_able_to_vote(voted_answer)
+    end
+
+    it 'user already voted and able to cancel' do
+      vote.reload
+
+      expect(user).to be_able_to_cancel_vote(voted_answer)
+    end
+
+    it 'user did\'t vote and tries to cancel' do
+      expect(user).to_not be_able_to_cancel_vote(question)
+    end
+  end
 end

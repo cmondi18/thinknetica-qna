1516f0ea7e85b9114ea18cfd3196a47867cd4f51
cmondi18 <disivolap@gmail.com>
2022-06-06 13:58:03 +0300

    specs: add sign in with github

---
+++ spec/support/omniauth_helpers.rb
@@ -0,0 +1,19 @@
+module OmniauthHelpers
+  def mock_auth_hash
+    OmniAuth.config.mock_auth[:github] = OmniAuth::AuthHash.new({
+      'provider' => 'github',
+      'uid' => '123545',
+      'info' => {
+        'email' => 'mockuser@mail.ru'
+      },
+      'user_info' => {
+        'name' => 'mockuser',
+        'image' => 'mock_user_thumbnail_url'
+      },
+      'credentials' => {
+        'token' => 'mock_token',
+        'secret' => 'mock_secret'
+      }
+    })
+  end
+end

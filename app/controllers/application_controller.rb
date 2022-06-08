741aff0ea581eb54566021c6ced546a359fbbd43
cmondi18 <disivolap@gmail.com>
2022-06-08 22:02:15 +0300

    feat: cancancan base setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :gon_user, unless: :devise_controller?
 
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, alert: exception.message
+  end
+
   private
 
   def gon_user
     # -1 value for non-auth users
     gon.user_id = current_user&.id || -1
   end
+
+  check_authorization
 end
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,6 +8,8 @@ class QuestionsController < ApplicationController
 
   after_action :publish_question, only: %i[create]
 
+  authorize_resource
+
   def index
     @questions = Question.all
   end

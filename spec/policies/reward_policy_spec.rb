d140585760bbfd384a8d0f149fbf2430f8e2d327
cmondi18 <disivolap@gmail.com>
2022-06-13 12:25:52 +0300

    fix: typo in spec

--- spec/policies/reward_policy_spec.rb
+++ spec/policies/reward_policy_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
-RSpec.describe QuestionPolicy, type: :policy do
+RSpec.describe RewardPolicy, type: :policy do
   let(:user) { create(:user) }
 
   subject { described_class }
 
-  permissions :create? do
+  permissions :index? do
     it 'grants access if user is exists' do
       expect(subject).to permit(user)
     end

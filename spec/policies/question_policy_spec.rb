4a1afc6e36dc73749970a792f4f6bf0f28405a88
cmondi18 <disivolap@gmail.com>
2022-06-13 13:17:55 +0300

    feat: authorize commentable

--- spec/policies/question_policy_spec.rb
+++ spec/policies/question_policy_spec.rb
@@ -72,4 +72,14 @@ RSpec.describe QuestionPolicy, type: :policy do
       expect(subject).to_not permit(nil, create(:question))
     end
   end
+
+  permissions :comment? do
+    it 'grants access if user is exists' do
+      expect(subject).to permit(user)
+    end
+
+    it 'denies access if user is guest' do
+      expect(subject).to_not permit(nil)
+    end
+  end
 end

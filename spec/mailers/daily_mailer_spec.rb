1369abd9e7197b6a15187ddd0267372e1f224d2a
cmondi18 <disivolap@gmail.com>
2022-06-20 17:07:09 +0300

    specs: increase coverage

--- spec/mailers/daily_mailer_spec.rb
+++ spec/mailers/daily_mailer_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe DailyMailer, type: :mailer do
   describe "digest" do
     let(:user) { create(:user) }
     let!(:questions) { create_list(:question, 3)}
+    let!(:old_question) { create(:question, title: 'Old question', created_at: Date.today.prev_day)}
     let(:mail) { DailyMailer.digest(user) }
 
     it "renders the headers" do
@@ -12,12 +13,16 @@ RSpec.describe DailyMailer, type: :mailer do
       expect(mail.from).to eq(["from@example.com"])
     end
 
-    it "renders the body" do
+    it "renders the body with today questions" do
       expect(mail.body.encoded).to match("Today created questions digest")
 
       questions.each do |question|
         expect(mail.body.encoded).to match(question.title)
       end
+    end
+
+    it 'does not render old question' do
+      expect(mail.body.encoded).to_not match(old_question.title)
     end
   end
 end

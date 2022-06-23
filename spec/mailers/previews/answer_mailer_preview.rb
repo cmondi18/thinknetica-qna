2c3ae1b0c0544cfc5b194849975ee2ebd10705e0
cmondi18 <disivolap@gmail.com>
2022-06-23 23:06:05 +0300

    feat: send notify to all subscribers

--- spec/mailers/previews/answer_mailer_preview.rb
+++ spec/mailers/previews/answer_mailer_preview.rb
@@ -1,9 +1,8 @@
 # Preview all emails at http://localhost:3000/rails/mailers/answer
-class AnswerPreview < ActionMailer::Preview
+class AnswerMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/answer/send_answer
   def send
     AnswerMailer.new_answer
   end
-
 end
